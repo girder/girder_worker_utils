36a95e6cd5191df7ef1efd84c899e5a5a1332755
Christopher Kotfila <chris.kotfila@kitware.com>
2018-01-31 15:42:56 -0500

    Add release CircleCI job

--- setup.py
+++ setup.py
@@ -6,9 +6,29 @@ from setuptools import find_packages, setup
 with open('requirements.in') as f:
     install_requires = f.readlines()
 
+
+def prerelease_local_scheme(version):
+    """Return local scheme version unless building on master in CircleCI.
+
+    This function returns the local scheme version number
+    (e.g. 0.0.0.dev<N>+g<HASH>) unless building on CircleCI for a
+    pre-release in which case it ignores the hash and produces a
+    PEP440 compliant pre-release version number (e.g. 0.0.0.dev<N>).
+
+    """
+
+    from setuptools_scm.version import get_local_node_and_date
+
+    if 'CIRCLE_BRANCH' in os.environ and \
+       os.environ['CIRCLE_BRANCH'] == 'master':
+        return ''
+    else:
+        return get_local_node_and_date(version)
+
+
 setup(
     name='girder-worker-utils',
-    use_scm_version=True,
+    use_scm_version={'local_scheme': prerelease_local_scheme},
     setup_requires=['setuptools_scm'],
     description='Helper utilities for the Girder Worker',
     author='Kitware, Inc.',
