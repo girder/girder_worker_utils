d65165d53578c86ce966a0add50a752c8299860b
Zach Mullen <zach.mullen@kitware.com>
2018-08-16 12:04:35 -0400

    Job artifact upload transform for Girder

--- girder_worker_utils/transforms/girder_io.py
+++ girder_worker_utils/transforms/girder_io.py
@@ -1,8 +1,10 @@
+import mimetypes
 import os
 import shutil
 import tempfile
 
 from girder_client import GirderClient
+from six.moves.urllib.parse import urlencode
 
 from ..transform import ResultTransform, Transform
 
@@ -117,3 +119,30 @@ class GirderUploadToFolder(GirderClientResultTransform):
                 shutil.rmtree(self.output_file_path)
             else:
                 os.remove(self.output_file_path)
+
+
+class GirderUploadJobArtifact(GirderClientResultTransform):
+    def __init__(self, job_id, **kwargs):
+        super(GirderUploadJobArtifact, self).__init(**kwargs)
+        self.job_id = job_id
+
+    def _repr_model_(self):
+        return "{}('{}')".format(self.__class__.__name__, self.job_id)
+
+    def _upload_artifact(self, file):
+        qs = urlencode({
+            'name': os.path.basename(file),
+            'size': os.stat(file).st_size,
+            'mimeType': mimetypes.guess_type(file)[0]
+        })
+        with open(file, 'rb') as fh:
+            self.gc.post('job/%s/artifact?%s' % (self.job_id, qs), data=fh)
+
+    def transform(self, path):
+        if os.path.isdir(path):
+            for f in os.listdir(path):
+                f = os.path.join(path, f)
+                if os.path.isfile(f):
+                    self._upload_artifact(f)
+        elif os.path.isfile(path):
+            self._upload_artifact(path)
